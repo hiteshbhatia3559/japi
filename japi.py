7009588a678a1f80d12edf6c2b0ef1304abe14b6
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-03-07 16:03:31 +0530

    Completed project, awaiting revisions.

---
+++ japi.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries  # Alpha_vantage_wrapper
+
+API_KEY = '6N6NCHHOY72NXZ4T'
+
+
+def getStockdata(symbol):
+    try:
+        print("Please wait, looking for data\n")
+        ts = TimeSeries(key=API_KEY, output_format='pandas')
+
+        data, meta_data = ts.get_intraday(symbol=symbol, interval='1min')
+
+        return str(data.tail(1).iloc[0]['4. close'])
+
+    except:
+        return "not found"
+
+
+def main():
+    while 1:
+        user_input = input("Type the name of the symbol to get the last 1min price or quit to exit\n").upper()
+        if user_input != "QUIT":
+            print('The current price of {} is {}\n'.format(user_input,getStockdata(user_input)))
+        else:
+            raise SystemExit
+
+main()
